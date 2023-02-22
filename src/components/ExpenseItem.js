a3f66403ae574edccdc7d0150c85c4919ed99f1a
amant2904 <tiwariaman2904@gmail.com>
2023-02-23 00:22:53 +0530

    using javascript variable inside JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import "./expenseItem.css"
 
 export default function ExpenseItem() {
+    const expenseDate = new Date(23, 10, 2023);
+    const expenseItem = "food"
+    const expenseCost = 234.67
+    const locationOfExpenditure = "KFC"
     return (
         <>
-            <h1>Expense Items</h1>
-            <p>Food Rs. 10</p>
-            <p>Petrol Rs. 100</p>
-            <p>Movies Rs. 200</p>
+            <div className='main_div'>
+                <h1>{expenseDate.toISOString()}</h1>
+                <h1>{expenseItem}</h1>
+                <h1>{expenseCost}</h1>
+                <h1>{locationOfExpenditure}</h1>
+            </div>
         </>
     )
 }
